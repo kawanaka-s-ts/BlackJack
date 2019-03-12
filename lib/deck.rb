0113a5fc0a7a0d49bfb5c568dd425456fba3ee21
kawanaka <kawanaka_s@tenso.com>
2019-03-12 21:33:13 +0900

    fixup! fixup! Deckクラス作り直し
    - テスト項目変更（インスタンス作成回数をカウント）

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,18 +1,12 @@
 class Deck
-  attr_reader :cards
-  def initialize
-    # カードの束
-    @cards = []
-  end
   
   # 52枚のカードを持った山札を作成
   def build
+    cards = []
     # 図柄と数字を組み合わせる
     ['♠', '♥', '♦', '♣'].product([*1..13]) do |s, n|
       # カードの作成（引数に図柄と数字を渡す）
-      card = Card.new(s, n)
-      # 作成したカードを山札に格納
-      cards << card.show_card
+      cards << Card.new(s, n)
     end
     # 山札を返す
     cards
