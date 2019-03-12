6575e613ee341314f9057154533b4b356eb9cf79
kawanaka <kawanaka_s@tenso.com>
2019-03-12 09:40:27 +0900

    Cardクラス作り直し

--- lib/card.rb
+++ lib/card.rb
@@ -1,9 +1,12 @@
 class Card
-  attr_accessor :numbers, :suit_num
-  def initialize
-    # カードの数値
-    @numbers = [*1..13]
-    # 図柄ごとの番号
-    @suit_num = [1, 2, 3, 4]
+  attr_accessor :suit, :number
+  def initialize(suit, number)
+    @suit = suit
+    @number = number
+  end
+
+  def show_card
+    puts ("#{suit}""#{number}")
   end
 end
+
---
+++ spec/card_spec.rb
@@ -0,0 +1,11 @@
+require "card"
+require "spec_helper"
+
+describe Card do
+  let(:card){Card.new("♠", "1")}
+  describe "#show_card" do
+    it "カードの柄と数字を表示する" do
+      expect(card.show_card).to eq "♠1"
+    end
+  end
+end
