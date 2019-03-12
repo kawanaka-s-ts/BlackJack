7046ac0bdca8bf6ac023ffef9a970ccab8961106
kawanaka <kawanaka_s@tenso.com>
2019-03-12 10:26:05 +0900

    Deckクラス作り直し

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,23 +1,16 @@
-require "card"
-
-class Deck < Card
-  # ゲームに使用するdeckを作成する
-  def create_deck
-    deck = []
-    # 図柄の要素数分、処理を繰り返す
-    suit_num.each do |suit|
-      # 数字の要素数分、処理を繰り返す
-      numbers.each do |num|
-        # 図柄の番号、数字の2つの要素を持った配列をdeckに入れる
-        deck.push([suit, num])
-      end
-    end
-    # 52枚のカードの入ったdeckを返す
-    deck
+class Deck
+  attr_accessor :numbers, :suit, :cards
+  def initialize
+    # カードの数値
+    @numbers = [*1..13]
+    # 図柄
+    @suit = ['♠', '♥', '♦', '♣']
+    # カードの束
+    @cards = []
   end
 
-  # deckをシャッフルする
-  def shuffle_deck(deck)
-    deck.shuffle!
+  # カードを作成する
+  def create_cards
+    cards = suit.product(numbers)
   end
 end
