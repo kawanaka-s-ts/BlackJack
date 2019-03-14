752e2c196bef544b0e2de0e6ccb5adaa2bc0781e
kawanaka <kawanaka_s@tenso.com>
2019-03-14 10:35:27 +0900

    得点への変換を行うメソッド追加

--- lib/user_base.rb
+++ lib/user_base.rb
@@ -1,4 +1,6 @@
 class UserBase
+  attr_accessor :hand_cards
+
   def initialize
     @hand_cards = []
   end
@@ -6,6 +8,26 @@ class UserBase
   # 引いたカードを手札に加える
   def draw(deck)
     # 手札に加える（渡されるのは、山札の先頭の要素）
-    @hand_cards.push(deck.drawCard)
+    hand_cards.push(deck.drawCard)
+  end
+
+  # 手札カードの数字を得点に変換
+  def convert_total_score
+    # 得点を入れていく配列
+    user_score = []
+    # 手札の要素数分、繰り返す
+    hand_cards.each do |card|
+      # 数字が10以下の時
+      if card.show_card_number < 10
+        # そのままの数値を格納する
+        user_score.push(card.show_card_number)
+      # 数字が10より大きい時
+      else
+        # 10を格納する
+        user_score.push(10)
+      end
+    end
+    # 変換した値の合計得点を返す
+    user_score.sum
   end
 end
--- spec/user_base_spec.rb
+++ spec/user_base_spec.rb
@@ -5,10 +5,27 @@ require "spec_helper"
 describe UserBase do
   let(:user_base) { UserBase.new }
   let(:deck) { Deck.new }
+  
   describe "#draw" do
     it "引いたカードを手札に加える" do
       deck.instance_variable_set(:@cards, ["♠1", "♠2", "♠3"])
       expect(user_base.draw(deck)).to eq ["♠1"]
+    end
+  end
+
+  describe "#convert_score" do
+    context "カードの数字が10以上の場合" do
+      it "10として扱う" do
+        user_base.instance_variable_set(:@hand_cards, [Card.new(1, 11)])
+        expect(user_base.convert_total_score).to eq 10
+      end
+    end
+
+    context "手札が複数ある場合" do
+      it "変換した値の合計得点を返す" do
+        user_base.instance_variable_set(:@hand_cards, [Card.new(1, 1), Card.new(1, 2), Card.new(1, 12)])
+        expect(user_base.convert_total_score).to eq 13
+      end
     end
   end
 end
