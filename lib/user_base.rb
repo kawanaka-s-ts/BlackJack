b78f277cdc29f22b8d8b2e5f0f1b8b6da1c9f9be
kawanaka <kawanaka_s@tenso.com>
2019-03-15 17:25:51 +0900

    mapメソッドを使った形に変更

--- lib/user_base.rb
+++ lib/user_base.rb
@@ -8,22 +8,19 @@ class UserBase
   # 引いたカードを手札に加える
   def draw(deck)
     # 手札に加える（渡されるのは、山札の先頭の要素）
-    hand_cards.push(deck.drawCard)
+    hand_cards.push(deck.draw_card)
   end
 
   # 手札を表示する
   def show_hand_cards
-    # 表示するための配列
-    view = []
     # 手札の要素数分、繰り返す
-    hand_cards.each do |card|
+    hand_cards.map do |card|
       # カードの柄を取得
       suit = card.suit
       # カードのランクを取得
       rank = card.rank
       # 柄とランクを足したものを格納
-      view.push("#{suit}#{rank}")
+      ("#{suit}#{rank}")
     end
-    view
   end
 end
