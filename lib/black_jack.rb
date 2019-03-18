443ab46eb29f9d80739f44f9ae5dd755e883ac1d
kawanaka <kawanaka_s@tenso.com>
2019-03-18 11:07:03 +0900

    得点への変換はBlackJackクラスで定義

--- lib/black_jack.rb
+++ lib/black_jack.rb
@@ -1,14 +1,41 @@
 class BlackJack
+
+  # 手札カードの数字を得点に変換
+  def convert_total_score(user)
+    # 手札の要素を合計する
+    user.hand_cards.sum do |card|
+      case card.rank
+      # rankが"J", "Q", "K"の時
+      when "J", "Q", "K"
+        # 10として扱う
+        10
+      # rankが"A"の時
+      when "A"
+        # 1として扱う
+        1
+      # 上記以外の場合
+      else 
+        # rankを数値にする
+        card.rank.to_i
+      end
+    end
+  end
+
   # 勝敗判定の実装
-  def decision(player_cards, dealer_cards)
+  def decision(player, dealer)
+    player_score = convert_total_score(player)
+    dealer_score = convert_total_score(dealer)
     # playerの手札の合計が21を超えた場合
-    if player_cards.inject(:+) > 21
+    if player_score > 21
       '敗北'
+    # dealerの手札の合計が21を超えた場合
+    elsif dealer_score > 21
+      '勝利' 
     # playerの手札の合計が相手より高い場合
-    elsif (21 - player_cards.inject(:+)) < (21 - dealer_cards.inject(:+))
+    elsif (21 - player_score) < (21 - dealer_score)
       '勝利' 
     # playerとdealerの手札の合計が同じ場合
-    elsif (21 - player_cards.inject(:+)) == (21 - dealer_cards.inject(:+))
+    elsif (21 - player_score) == (21 - dealer_score)
       '引き分け'
     end
   end
--- spec/black_jack_spec.rb
+++ spec/black_jack_spec.rb
@@ -1,28 +1,62 @@
 require "black_jack"
+require "deck"
+require "card"
+require "player"
+require "dealer"
 require "spec_helper"
 
 describe BlackJack do
-  let(:black_jack){BlackJack.new}
-  let(:dealer_cards){[10, 10]}
+  let(:black_jack) { BlackJack.new }
+  let(:player) { Player.new }
+  let(:dealer) { Dealer.new }
+  let(:dealer_cards) { [10, 10] }
+
+  describe "#convert_score" do
+    context "カードの数字が10以上の場合" do
+      it "10として扱う" do
+        player.instance_variable_set(:@hand_cards, [Card.new("♠", "J")])
+        expect(black_jack.convert_total_score(player)).to eq 10
+      end
+    end
+
+    context "手札が複数ある場合" do
+      it "変換した値の合計得点を返す" do
+        player.instance_variable_set(:@hand_cards, [Card.new("♠", "A"), Card.new("♠", "2"), Card.new("♠", "Q")])
+        expect(black_jack.convert_total_score(player)).to eq 13
+      end
+    end
+  end
+
   describe "#decision" do
     context "playerの手札の合計が相手より21に近い場合" do
-      it "'勝利'と表示する" do
-        player_cards = [10, 11]
-        expect(black_jack.decision(player_cards, dealer_cards)).to eq '勝利'
+      it "勝利と表示する" do
+        player.instance_variable_set(:@hand_cards, [Card.new("♠", "2")])
+        dealer.instance_variable_set(:@hand_cards, [Card.new("♠", "A")])
+        expect(black_jack.decision(player, dealer)).to eq '勝利'
       end
     end
 
     context "playerと相手の手札の合計が同じ場合" do
-      it "'引き分け'と表示する" do
-        player_cards = [10, 10]
-        expect(black_jack.decision(player_cards, dealer_cards)).to eq '引き分け'
+      it "引き分けと表示する" do
+        player.instance_variable_set(:@hand_cards, [Card.new("♠", "A")])
+        dealer.instance_variable_set(:@hand_cards, [Card.new("♠", "A")])
+        expect(black_jack.decision(player, dealer)).to eq '引き分け'
       end
     end
 
     context "playerの手札の合計が21を超えた場合" do
-      it "'敗北'と表示する" do
-        player_cards = [10, 10, 2]
-        expect(black_jack.decision(player_cards, dealer_cards)).to eq '敗北'
+      it "敗北と表示する" do
+        player.instance_variable_set(:@hand_cards, [Card.new("♠", "J"), Card.new("♠", "Q"), Card.new("♠", "2")])
+        dealer.instance_variable_set(:@hand_cards, [Card.new("♠", "A")])
+        expect(black_jack.decision(player, dealer)).to eq '敗北'
+      end
+    end
+
+    context "dealerの手札の合計が21を超えた場合" do
+      it "勝利と表示する" do
+        player.instance_variable_set(:@hand_cards, [Card.new("♠", "A")])
+        dealer.instance_variable_set(:@hand_cards, [Card.new("♠", "J"), Card.new("♠", "Q"), Card.new("♠", "2")])
+        expect(black_jack.decision(player, dealer)).to eq '勝利'
       end
     end
   end
