d49642371fe8382964b8f358e6ae22cd8a123cfa
kawanaka <kawanaka_s@tenso.com>
2019-03-19 09:26:41 +0900

    テストでsubjectを使えるように練習

--- spec/black_jack_spec.rb
+++ spec/black_jack_spec.rb
@@ -8,31 +8,33 @@ require "spec_helper"
 describe BlackJack do
   let(:black_jack) { BlackJack.new }
   let(:player) { Player.new(Card.new("♠", "A"), Card.new("♠", "2")) }
-  let(:dealer) { Dealer.new }
+  let(:dealer) { Dealer.new(Card.new("♠", "A"), Card.new("♠", "2")) }
   let(:dealer_cards) { [10, 10] }
 
   describe "#convert_score" do
+    subject { black_jack.convert_total_score(player) }
     context "カードの数字が10以上の場合" do
       it "10として扱う" do
         player.instance_variable_set(:@hand_cards, [Card.new("♠", "J")])
-        expect(black_jack.convert_total_score(player)).to eq 10
+        is_expected.to eq 10
       end
     end
 
     context "手札が複数ある場合" do
       it "変換した値の合計得点を返す" do
         player.instance_variable_set(:@hand_cards, [Card.new("♠", "A"), Card.new("♠", "2"), Card.new("♠", "Q")])
-        expect(black_jack.convert_total_score(player)).to eq 13
+        is_expected.to eq 13
       end
     end
   end
 
   describe "#decision" do
+    subject { black_jack.decision(player, dealer) }
     context "playerの手札の合計が相手より21に近い場合" do
       it "勝利と表示する" do
         player.instance_variable_set(:@hand_cards, [Card.new("♠", "2")])
         dealer.instance_variable_set(:@hand_cards, [Card.new("♠", "A")])
-        expect(black_jack.decision(player, dealer)).to eq '勝利'
+        is_expected.to eq '勝利'
       end
     end
 
@@ -40,7 +42,7 @@ describe BlackJack do
       it "引き分けと表示する" do
         player.instance_variable_set(:@hand_cards, [Card.new("♠", "A")])
         dealer.instance_variable_set(:@hand_cards, [Card.new("♠", "A")])
-        expect(black_jack.decision(player, dealer)).to eq '引き分け'
+        is_expected.to eq '引き分け'
       end
     end
 
@@ -48,7 +50,7 @@ describe BlackJack do
       it "敗北と表示する" do
         player.instance_variable_set(:@hand_cards, [Card.new("♠", "J"), Card.new("♠", "Q"), Card.new("♠", "2")])
         dealer.instance_variable_set(:@hand_cards, [Card.new("♠", "A")])
-        expect(black_jack.decision(player, dealer)).to eq '敗北'
+        is_expected.to eq '敗北'
       end
     end
 
@@ -56,7 +58,7 @@ describe BlackJack do
       it "勝利と表示する" do
         player.instance_variable_set(:@hand_cards, [Card.new("♠", "A")])
         dealer.instance_variable_set(:@hand_cards, [Card.new("♠", "J"), Card.new("♠", "Q"), Card.new("♠", "2")])
-        expect(black_jack.decision(player, dealer)).to eq '勝利'
+        is_expected.to eq '勝利'
       end
     end
   end
