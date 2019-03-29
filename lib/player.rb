9b37e2078e19e6ec8cdab7e1be0f7b5f6aaeaba8
kawanaka <kawanaka_s@tenso.com>
2019-03-29 10:06:02 +0900

    UserBaseクラス削除、Playerクラス作り直し
    - player_actionはBlackJackクラスへ移動

--- lib/player.rb
+++ lib/player.rb
@@ -1,34 +1,15 @@
-require_relative "user_base"
-
-class Player < UserBase
+class Player
   attr_accessor :hand_cards
 
   def initialize(first_card, second_card)
     @hand_cards = [first_card, second_card]
   end
 
-  # プレーヤーの動作
-  def action(deck)
-    puts "ドローしますか? y/n"
-    action = STDIN.gets.chomp
-    # 入力がyの時
-    if action == "y"
-      puts "ドローします"
-      # ドローする
-      draw(deck)
-      puts "現在の手札は #{show_hand_cards} です"
-      # trueを返す
-      return true
-    # 入力がnの時
-    elsif action == "n"
-      puts "ドローしませんでした"
-      # falseを返す
-      return false
-    # 入力がyでもnでもない時
-    else
-      puts "無効な文字列が入力されました、再度入力してください y/n"
-      # trueを返す
-      return true
-    end
+  # 手札を表示する
+  def show_hand_cards
+    # 手札の要素数分、繰り返す
+      hand_cards.map do |card|
+      card.display
+    end.join(" , ")
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,37 +4,11 @@ require "spec_helper"
 
 describe Player do
   let(:player) { Player.new(Card.new("♠", "A"), Card.new("♠", "2")) }
-  let(:deck) { Deck.new }
 
-  describe "#action" do
-    context "yが入力された時" do
-      it "カードを引く" do
-        deck.instance_variable_set(:@cards, [Card.new("♠", "A")])
-        player.instance_variable_set(:@hand_cards, [])
-        allow(STDIN).to receive(:gets).and_return "y"
-        expect(STDOUT).to receive(:puts).with("ドローしますか? y/n")
-        expect(STDOUT).to receive(:puts).with("ドローします")
-        expect(STDOUT).to receive(:puts).with("現在の手札は ♠A です")
-        player.action(deck)
-      end
-    end
-
-    context "nが入力された時" do
-      it "カードを引く" do
-        allow(STDIN).to receive(:gets).and_return "n"
-        expect(STDOUT).to receive(:puts).with("ドローしますか? y/n")
-        expect(STDOUT).to receive(:puts).with("ドローしませんでした")
-        player.action(deck)
-      end
-    end
-
-    context "yが入力された時" do
-      it "カードを引く" do
-        allow(STDIN).to receive(:gets).and_return "a"
-        expect(STDOUT).to receive(:puts).with("ドローしますか? y/n")
-        expect(STDOUT).to receive(:puts).with("無効な文字列が入力されました、再度入力してください y/n")
-        player.action(deck)
-      end
+  describe "#show_hand_cards" do
+    it "手札を表示する" do
+      player.instance_variable_set(:@hand_cards, [Card.new("♠", "A"), Card.new("♠", "2"), Card.new("♠", "Q")])
+      expect(player.show_hand_cards).to eq "♠A , ♠2 , ♠Q"
     end
   end
 end
